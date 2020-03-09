f292075afd79494a2d5b3319d369536b61ada296
yo-p <papapapipipi42@gmail.com>
2020-03-09 06:14:55 +0000

    finish bookers2-debug

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+
+  def show
+	  @book = Book.find(params[:id])
+	  @new_book = Book.new
+	  @user = @book.user
+  end
+
+  def index
+	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+	@book = Book.new
+	@user = current_user
+	  
+  end
+
+  def create
+	  @book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+	  @book.user_id = current_user.id
+  	if @book.save #入力されたデータをdbに保存する。
+		flash[:notice] = "successfully created book!"#保存された場合の移動先を指定。
+		redirect_to book_path(@book) 
+	  else
+		# @book = Book.new
+		@user = current_user
+  		@books = Book.all
+  		render 'index'
+  	end
+  end
+
+  def edit
+	@book = Book.find(params[:id])
+	@user = current_user
+	  if @book.user_id != current_user.id
+		redirect_to books_path
+	  end
+  end
+
+
+
+  def update
+  	@book = Book.find(params[:id])
+	  if @book.update(book_params)
+		flash[:notice] = "successfully updated book!"
+  		redirect_to book_path
+	  else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+		@user = current_user
+  		render "edit"
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path, notice: "successfully delete book!"
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title,:body)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+	before_action :baria_user, only: [:update]
+
+  def show
+  	@user = User.find(params[:id])
+  	@books = @user.books
+  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
+
+  def index
+	  @users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
+	  @user = current_user
+		@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
+
+  def edit
+	  @user = User.find(params[:id])
+	  if @user.id != current_user.id
+		redirect_to user_path(current_user)
+	  end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+	  if @user.update(user_params)
+		flash[:notice] = "successfully updated user!"
+		  redirect_to user_path(@user) 
+		#   notice: "successfully updated user!"
+  	else
+  		render "edit"
+	  end
+
+	def destroy
+		flash[:notice] = "Signed out successfully"
+	end
+	  
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :email, :introduction, :profile_image)
+  end
+
+  #url直接防止　メソッドを自己定義してbefore_actionで発動。
+   def baria_user
+  	unless params[:id].to_i == current_user.id
+  		redirect_to user_path(current_user)
+  	end
+   end
+
+end
